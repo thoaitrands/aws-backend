8e300a7a94764dd317ad3db511fe3068092f497b
thoaitrands <thoaitrands@gmail.com>
2024-07-13 11:38:27 +0700

    Init repository

---
+++ src/main/java/com/training/aws_backend/AwsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.training.aws_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBackendApplication.class, args);
+	}
+
+}
